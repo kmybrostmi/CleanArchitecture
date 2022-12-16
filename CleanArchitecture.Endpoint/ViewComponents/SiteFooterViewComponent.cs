5b83e49a2efb42a573a2a71c4e5c48da2436c409
Kamyab <87451309+kmybrostmi@users.noreply.github.com>
2022-12-16 15:07:17 +0330

    Import Template

---
+++ CleanArchitecture.Endpoint/ViewComponents/SiteFooterViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Endpoint.ViewComponents;
+
+public class SiteFooterViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return View("SiteFooter");
+    }
+}
---
+++ CleanArchitecture.Endpoint/ViewComponents/SiteHeaderViewComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Endpoint.ViewComponents;
+public class SiteHeaderViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return View("SiteHeader");
+    }
+}
